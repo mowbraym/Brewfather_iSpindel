7855139f7374cf4b4f0daa3c34e0ff8e914a6d16
mowbraym <33804198+mowbraym@users.noreply.github.com>
2018-11-20 10:49:49 +1000

    Converted to iSpindel
    Changed cloned Tilt code to suit iSpindel devices

--- __init__.py
+++ __init__.py
@@ -16,60 +16,48 @@ DEBUG = False
 drop_first = None
 
 # Parameters
-brewfather_comment = None
-brewfather_id = None
+brewfather_iSPindel_id = None
 
 def log(s):
     if DEBUG:
-        s = "brewfather: " + s
+        s = "brewfather_iSPindel: " + s
         cbpi.app.logger.info(s)
 
 @cbpi.initalizer(order=9000)
 def init(cbpi):
-    cbpi.app.logger.info("brewfather plugin Initialize")
-    log("Brewfather params")
-# the comment that goes along with each post (visible in the edit data screen)
-    global brewfather_comment
+    cbpi.app.logger.info("brewfather_iSPindel plugin Initialize")
+    log("Brewfather_iSPindel params")
 # the unique id value (the bit following id= in the "Cloud URL" in the setting screen
-    global brewfather_id
+    global brewfather_iSPindel_id
 
-    brewfather_comment = cbpi.get_config_parameter("brewfather_comment", None)
-    log("Brewfather brewfather_comment %s" % brewfather_comment)
-    brewfather_id = cbpi.get_config_parameter("brewfather_id", None)
-    log("Brewfather brewfather_id %s" % brewfather_id)
+    brewfather_iSPindel_id = cbpi.get_config_parameter("brewfather_iSPindel_id", None)
+    log("Brewfather brewfather_iSPindel_id %s" % brewfather_iSPindel_id)
 
-    if brewfather_comment is None:
-	log("Init brewfather config Comment")
+    if brewfather_iSPindel_id is None:
+	log("Init brewfather_iSPindel config URL")
 	try:
 # TODO: is param2 a default value?
-	    cbpi.add_config_parameter("brewfather_comment", "", "text", "Brewfather comment")
+	    cbpi.add_config_parameter("brewfather_iSPindel_id", "", "text", "Brewfather_iSPindel id")
 	except:
-	    cbpi.notify("Brewfather Error", "Unable to update Brewfather comment parameter", type="danger")
-    if brewfather_id is None:
-	log("Init brewfather config URL")
-	try:
-# TODO: is param2 a default value?
-	    cbpi.add_config_parameter("brewfather_id", "", "text", "Brewfather id")
-	except:
-	    cbpi.notify("Brewfather Error", "Unable to update Brewfather id parameter", type="danger")
-    log("Brewfather params ends")
+	    cbpi.notify("Brewfather_iSPindel Error", "Unable to update Brewfather_iSPindel id parameter", type="danger")
+    log("Brewfather_iSPindel params ends")
 
 # interval=900 is 900 seconds, 15 minutes, same as the Tilt Android App logs.
 # if you try to reduce this, brewfather will throw "ignored" status back at you
-@cbpi.backgroundtask(key="brewfather_task", interval=900)
-def brewfather_background_task(api):
-    log("brewfather background task")
+@cbpi.backgroundtask(key="brewfather_iSPindel_task", interval=900)
+def brewfather_iSPindel_background_task(api):
+    log("brewfather_iSPindel background task")
     global drop_first
     if drop_first is None:
         drop_first = False
         return False
 
-    if brewfather_id is None:
+    if brewfather_iSPindel_id is None:
         return False
 
     now = datetime.datetime.now()
     for key, value in cbpi.cache.get("sensors").iteritems():
-	log("key %s value.name %s value.instance.last_value %s" % (key, value.name, value.instance.last_value))
+	log("key %s value.name %s value.instance.last_value %s value.type %s value.instance.sensorType %s" % (key, value.name, value.instance.last_value, value.type, value.instance.sensorType))
 #
 # TODO: IMPORTANT - Temp sensor must be defined preceeding Gravity sensor and 
 #		    each Tilt must be defined as a pair without another Tilt
@@ -79,34 +67,31 @@ def brewfather_background_task(api):
 #			PINK Temperature
 #			PINK Gravity
 #
-	if (value.type == "TiltHydrometer"):
+	if (value.type == "iSPindel"):
 	    if (value.instance.sensorType == "Temperature"):
 # A Tilt Temperature device is the first of the Tilt pair of sensors so
 #    reset the data block to empty
 		payload = "{ "
-# generate timestamp in "Excel" format
-		timepoint = now.toordinal() - 693594 + (60*60*now.hour + 60*now.minute + now.second)/float(24*60*60)
-		payload += " \"Timepoint\": \"%s\",\r\n" % timepoint
-		payload += " \"Color\": \"%s\",\r\n" % value.instance.color
-## TODO: would this work here? data['beer'] = cbpi.get_config_parameter("brewfather_%s" % value.instance.color, None)
-		payload += " \"Beer\": \"\",\r\n"
-# 2018.11.09 - End Changed code
+		payload += " \"name\": \"%s\",\r\n" % value.name
+		payload += " \"ID\": \"%s\",\r\n" % value.id
 		temp = value.instance.last_value
 # brewfather expects *F so convert back if we use C
 		if (cbpi.get_config_parameter("unit",None) == "C"):
 		    temp = value.instance.last_value * 1.8 + 32
-                payload += " \"Temp\": \"%s\",\r\n" % temp
-	    if (value.instance.sensorType == "Gravity"):
-                payload += " \"SG\": \"%s\",\r\n" % value.instance.last_value
-                payload += " \"Comment\": \"%s\" }" % cbpi.get_config_parameter("brewfather_comment", None)
+                payload += " \"temperature\": \"%s\",\r\n" % temp
+# might squeeze a battery device in here and make iSpindel a trio of logical devices
+		if (value.instance.sensorType == "Gravity"):
+                payload += " \"gravity\": \"%s\",\r\n" % value.instance.last_value
+                payload += " \"interval\": \"900\",\r\n"
+                payload += " \"RSSI\": \"-96\" }"
 		log("Payload %s" % payload)
-		url = "http://log.brewfather.net/tilt"
+		url = "http://log.brewfather.net/ispindel"
 		headers = {
 		    'Content-Type': "application/json",
 		    'Cache-Control': "no-cache"
 		    }
-		id = cbpi.get_config_parameter("brewfather_id", None)
+		id = cbpi.get_config_parameter("brewfather_iSpindel_id", None)
 		querystring = {"id":id}
 		r = requests.request("POST", url, data=payload, headers=headers, params=querystring)
 		log("Result %s" % r.text)
-    log("brewfather done")
+    log("brewfather_iSpindel done")
